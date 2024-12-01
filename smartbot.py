ec61cd2b1b9d295cbd5dc1e8c5d4585c00102a9f
Andromeda-pixel25 <v8057483@gmail.com>
2024-12-01 14:37:56 +0530

    Add files via upload

---
+++ smartbot.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyA7V6N800cWrvaW2hlgHazi62i4Gh-idZk")
+model = genai.GenerativeModel('gemini-pro')
+
+messages = model.start_chat()
+
+st.title("SMARTBOT")
+
+def role_to_streamlit(role):
+  if role == "model":
+    return "assistant"
+  else:
+    return role
+
+if "messages" not in st.session_state:
+    st.session_state.messages = model.start_chat(history = [])
+
+ 
+for message in st.session_state.messages.history:
+  role = role_to_streamlit(getattr(message, "role"))
+  text = ""
+  parts = getattr(message, "parts", None) #none is specified to run even if nothing exists
+  if parts:
+    for part in parts:  
+      if hasattr(part, "text"):  # Check if `text` attribute exists
+          text = part.text
+          break  
+
+        # display the history
+  with st.chat_message(role):
+    st.markdown(text)
+     
+
+prompt=st.chat_input("ask away..")
+if prompt:
+    st.chat_message("user").markdown(prompt)
+    response = st.session_state.messages.send_message(prompt) 
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
