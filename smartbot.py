2f72a0307c95f7a485ac6a2691d2f6a19d651679
Andromeda-pixel25 <v8057483@gmail.com>
2024-12-06 21:02:42 +0530

    Update smartbot.py

--- smartbot.py
+++ smartbot.py
@@ -15,6 +15,26 @@ st.markdown("Powered by Google Generative AI for Seamless Conversations")
 image="https://github.com/Andromeda-pixel25/smartbot-using-python/blob/main/letter-z%20(1).png?raw=true"
 st.logo(image,size="large")
 
+def speak(text):
+   engine.say(text)
+   engine.runAndWait()
+
+def listen():
+   recognizer = sr.Recognizer
+   with sr.Microphone() as source:
+      st.info("Listening.....")
+      try:
+         audio=recognizer.listen(source,timeout=5) #listen to input with 5sec timeout
+         command=recognizer.recognize_google(audio)
+         st.success(f"recognized: {command}")
+         return command
+      except sr.UnknownValueError:
+         st.error("Sorry, I couldn't understand that.")
+         return None
+      except sr.RequestError:
+          st.error("Voice recognition service is unavailable.")
+          return None 
+        
 def role_to_streamlit(role):
   if role == "model":
     return "assistant"
